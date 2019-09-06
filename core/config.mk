401663600fd7a31749338f761dfe3de0b2ac8586
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include AEX SEpolicy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1176,9 +1176,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
 
 ifneq ($(AOSP_BUILD),)
+ifneq ($(wildcard device/aosp/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/aosp/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file
