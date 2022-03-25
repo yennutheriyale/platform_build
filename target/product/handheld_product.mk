b2605bfe70346312e00bea8d421fcd7aa5c3eab9
dlwlrma123 <alexfinhart@gmail.com>
2022-03-25 14:26:39 +0800

    product: remove some aosp prebuilt apps
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -23,12 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 # /product packages
 PRODUCT_PACKAGES += \
     Calendar \
-    Camera2 \
     Contacts \
     DeskClock \
-    Gallery2 \
     LatinIME \
-    Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
     SettingsIntelligence \
